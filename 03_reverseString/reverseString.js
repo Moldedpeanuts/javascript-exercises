0d9ce4e8b9fa8ec965605d91c65c82851fbcd26d
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-11-22 15:22:30 +0200

    Join all three methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,6 @@
 const reverseString = function(string) {
 
-    let splitString = string.split("");
-
-    let reverseArray = splitString.reverse();
-
-    let joinArray = reverseArray.join("");
-
-    return joinArray;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
