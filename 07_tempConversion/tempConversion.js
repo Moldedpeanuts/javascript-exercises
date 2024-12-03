e0d50f93b6cdb6d059fae0e22f89bd953fc611d9
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-12-03 10:21:29 +0200

    Add parseFloat and toFixed methods to output a decimal 1 accurate float number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const convertToCelsius = function(tempF) {
-  return tempF = (tempF - 32) * 5 / 9;
+const convertToCelsius = function(tempC) {
+  tempC = (tempC - 32) * 5 / 9;
+  return parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function(tempC) {
-  return tempC = tempC * 9 / 5 + 32;
+const convertToFahrenheit = function(tempF) {
+  tempF = tempF * 9 / 5 + 32;
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
