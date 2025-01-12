255cb56b5382b874cecbd731253849873eb5efc0
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-12 17:29:19 +0200

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanString = str.toLowerCase().split('').filter((char) => alphabet.includes(char)).join('');
+
+    const reverseStr = cleanString.split('').reverse().join('');
+
+    return cleanString === reverseStr;
 };
 
 // Do not edit below this line
