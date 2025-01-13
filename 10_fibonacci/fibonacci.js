a3763e5cd427925043410bbbc8273495991517e1
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-13 13:58:21 +0200

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,27 +1,26 @@
-const fibonacci = function(num) {
-    if(num < 0) {
-        return "OOPS";
+const fibonacci = function(arg) {
+    let num;
+
+    if(typeof arg !== "number") {
+        num = parseInt(arg);
+    } else {
+        num = arg;
     }
 
-    if(num === "1" ||  num === "2") {
-        return 1;
-    }
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
 
-    if (num === "0") {
-        return 0;
-    }
-
+    // we are starting in position 2, where the current number is 1 and previous number is 1 and second to last previous is 0.
     let prev1 = 1;
-    let prev2 = 1;
-    let current = 0;
-    
-    for(let i=3; i<=num; i++) {
-        current = prev1 + prev2;
+    let prev2 = 0;
+
+    for(let i = 2; i <= num; i++) {
+        let current = prev1 + prev2;
         prev2 = prev1;
         prev1 = current;
     }
 
-    return current;
+    return prev1;
 };
 
 // Do not edit below this line
