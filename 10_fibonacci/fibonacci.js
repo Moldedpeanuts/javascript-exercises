d7f162968bdc8e066f633029f6e7a7cd7c8463ca
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-13 13:03:47 +0200

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) {
+        return "OOPS";
+    }
 
+    if(num === "1" ||  num === "2") {
+        return 1;
+    }
+
+    if (num === "0") {
+        return 0;
+    }
+
+    let prev1 = 1;
+    let prev2 = 1;
+    let current = 0;
+    
+    for(let i=3; i<=num; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
