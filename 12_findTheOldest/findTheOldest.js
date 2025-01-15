1870e1817873ee82e734df24df48ff9af03a4034
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-15 11:55:04 +0200

    Finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const aged = people.reduce((oldest, person) => {
+        let yearDifference;
+        const currentYear = new Date().getFullYear();
+        if(person.yearOfDeath) {
+            yearDifference = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            yearDifference = currentYear - person.yearOfBirth;
+        }
 
+        let oldestYearDifference = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+
+        if(yearDifference > oldestYearDifference) {
+            oldest = person;
+        }
+
+        return oldest;
+    }, people[0]);
+
+    return aged;
 };
 
 // Do not edit below this line
